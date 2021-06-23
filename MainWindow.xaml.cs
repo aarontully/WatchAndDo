ddc24d1bece7312f925104917f03ab8dd24fb925
Aaron Tully <aarontully0@gmail.com>
2021-06-23 15:52:00 +1000

    Created XAML and main C#

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,18 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Forms;
+using System.Windows.Threading;
+using Timer = System.Windows.Forms.Timer;
 
 namespace WatchAndDo
 {
@@ -21,77 +14,248 @@ namespace WatchAndDo
     /// </summary>
     public partial class MainWindow : Window
     {
+        FileSystemWatcher watcher;
+        static readonly object locker = new();
+        private readonly Timer timer = new();
+        private bool isWatching;
+        private bool canChange;
+        private string filePath = string.Empty;
+        DateTime lastRead = DateTime.MinValue;
+
         public MainWindow()
         {
             InitializeComponent();
 
-            Main();
-        }
-
-        private void Main()
-        {
-            using var watcher = new FileSystemWatcher(@"C:\Test\TestFolder");
-
-            watcher.NotifyFilter = NotifyFilters.Attributes;
-            watcher.NotifyFilter = NotifyFilters.CreationTime;
-            watcher.NotifyFilter = NotifyFilters.DirectoryName;
-            watcher.NotifyFilter = NotifyFilters.FileName;
-            watcher.NotifyFilter = NotifyFilters.LastAccess;
-            watcher.NotifyFilter = NotifyFilters.LastWrite;
-            watcher.NotifyFilter = NotifyFilters.Security;
-            watcher.NotifyFilter = NotifyFilters.Size;
-
-            watcher.Changed += OnChanged;
-            watcher.Created += OnCreated;
-            watcher.Deleted += OnDeleted;
-            watcher.Renamed += OnRenamed;
-            watcher.Error += OnError;
-
-            watcher.Filter = " *.*";
-            watcher.IncludeSubdirectories = true;
-            watcher.EnableRaisingEvents = true;
-        }
-
-        private void OnError(object sender, ErrorEventArgs e)
-        {
-            PrintException(e.GetException());
-        }
-
-        private void PrintException(Exception ex)
-        {
-            if(ex != null)
+            if (!string.IsNullOrEmpty(Settings.Default.PathSetting))
             {
-                OutputText.Text = $"Message: {ex.Message}";
-                OutputText.Text = "Stacktrace";
-                OutputText.Text = ex.StackTrace;
+                TxtDir.Text = Settings.Default.PathSetting; //gets the saved path
+                ListDirectory(treeFiles, TxtDir.Text);
             }
         }
 
-        private void OnRenamed(object sender, RenamedEventArgs e)
+        private void BtnBrowse_Click(object sender, RoutedEventArgs e)
         {
-            OutputText.Text = $"Renamed: ";
-            OutputText.Text = $"    Old: {e.OldFullPath}";
-            OutputText.Text = $"    New: {e.FullPath}";
+            var dialog = new FolderBrowserDialog();
+
+            if(dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                TxtDir.Text = dialog.SelectedPath;
+            }
+            ListDirectory(treeFiles, TxtDir.Text);
         }
 
-        private void OnDeleted(object sender, FileSystemEventArgs e)
+        //This method will list files to our treeview
+        private void ListDirectory(System.Windows.Controls.TreeView treeView, string path)
         {
-            OutputText.Text = $"Deleted: {e.FullPath}";
+            try
+            {
+                treeView.Items.Clear();
+                var rootDirectoryInfo = new DirectoryInfo(path);
+                treeView.Items.Add(CreateDirectoryItems(rootDirectoryInfo));
+            }
+            catch (Exception ex)
+            {
+                AppendListViewCalls(ex.Message);
+            }
         }
 
-        private void OnCreated(object sender, FileSystemEventArgs e)
+        private static TreeViewItem CreateDirectoryItems(DirectoryInfo directoryInfo)
         {
-            string value = $"Created: {e.FullPath}";
-            OutputText.Text = value;
+            var directoryItem = new TreeViewItem { Header = directoryInfo.Name };
+            foreach(var directory in directoryInfo.GetDirectories())
+            {
+                directoryItem.Items.Add(CreateDirectoryItems(directory));
+            }
+            foreach(var file in directoryInfo.GetFiles())
+            {
+                directoryItem.Items.Add(new TreeViewItem { Header = file.Name, Tag = file.FullName });
+            }
+
+            return directoryItem;
+        }
+
+        private void BtnListen_Click(object sender, RoutedEventArgs e)
+        {
+            //Check to see if filewatchewr is on or not and display usefull info
+            if (isWatching)
+            {
+                BtnListen.Content = "Start Watching";
+                StopWatching();
+            }
+            else
+            {
+                BtnListen.Content = "Stop Watching";
+                StartWatching();
+            }
+        }
+
+        private void StartWatching()
+        {
+            if (!IsDirectoryValid(TxtDir.Text))
+            {
+                AppendListViewCalls(DateTime.Now + " - Watch Directory Invalid");
+                return;
+            }
+            isWatching = true;
+            timer.Enabled = true;
+            timer.Start();
+            timer.Interval = 500;
+            AppendListViewCalls(DateTime.Now + " - Watcher Started");
+
+            watcher = new FileSystemWatcher();
+            watcher.Path = TxtDir.Text;
+            watcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite | NotifyFilters.FileName | NotifyFilters.DirectoryName;
+            watcher.Filter = "*.*";
+            watcher.Created += new FileSystemEventHandler(OnChanged);
+            watcher.Renamed += new RenamedEventHandler(OnChanged);
+            watcher.Changed += new FileSystemEventHandler(OnChanged);
+            watcher.EnableRaisingEvents = true;
+        }
+
+        private void StopWatching()
+        {
+            isWatching = false;
+            timer.Enabled = false;
+            timer.Stop();
+            AppendListViewCalls(DateTime.Now + " - Watcher Stopped");
+        }
+
+        private static bool IsDirectoryValid(string path)
+        {
+            if (Directory.Exists(path))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
         private void OnChanged(object sender, FileSystemEventArgs e)
         {
-            if(e.ChangeType != WatcherChangeTypes.Changed)
+            //Specify what to do when a file is changed, created, or deleted  
+
+            //filter file types  
+            if (Regex.IsMatch(System.IO.Path.GetExtension(e.FullPath), @"\.txt", RegexOptions.IgnoreCase))
             {
-                return;
+                try
+                {
+                    while (IsFileLocked(e.FullPath))
+                    {
+                        System.Threading.Thread.Sleep(100);
+                    }
+
+                    lock (locker)
+                    {
+                        //Process file  
+                        //Do further activities  
+                        DateTime lastWriteTime = File.GetLastWriteTime(e.FullPath);
+                        if (lastWriteTime != lastRead)
+                        {
+                            AppendListViewCalls("File: \"" + e.FullPath + "\"- " + DateTime.Now + " - Processed the changes successfully");
+                            lastRead = lastWriteTime;
+                        }
+
+                    }
+                }
+                catch (FileNotFoundException)
+                {
+                    //Stop processing  
+                }
+                catch (Exception ex)
+                {
+                    AppendListViewCalls("File: \"" + e.FullPath + "\" ERROR processing file (" + ex.Message + ")");
+                }
             }
-            OutputText.Text = ($"Changed: {e.FullPath}");
+
+            else
+                AppendListViewCalls("File: \"" + e.FullPath + "\" has been ignored");
+        }
+
+        private static bool IsFileLocked(string file)
+        {
+            FileStream stream = null;
+
+            try
+            {
+                stream = new FileInfo(file).Open(FileMode.Open, FileAccess.ReadWrite, FileShare.None);
+            }
+            catch (FileNotFoundException)
+            {
+                throw;
+            }
+            catch (IOException)
+            {
+                //the file is unavailable because it is:  
+                //still being written to  
+                //or being processed by another thread  
+                //or does not exist (has already been processed)  
+                return true;
+            }
+            finally
+            {
+                if (stream != null)
+                    stream.Close();
+            }
+
+            //file is not locked  
+            return false;
+        }
+
+        private void AppendListViewCalls(object input)
+        {
+            this.Dispatcher.Invoke(DispatcherPriority.Normal, new Action(delegate ()
+            {
+                this.ListResults.Items.Add(input);
+
+            }));
+        }
+
+        private void treeFiles_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            try
+            {
+                var item = (TreeViewItem)e.NewValue;
+                filePath = item.Tag.ToString();
+
+                //Check changes for .txt files only  
+                if (Regex.IsMatch(System.IO.Path.GetExtension(filePath), @"\.txt", RegexOptions.IgnoreCase))
+                {
+                    canChange = false;
+                    TxtEditor.Clear();
+                    string contents = File.ReadAllText(filePath);
+                    TxtEditor.Text = contents;
+                    canChange = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                AppendListViewCalls(ex.Message);
+            }
+        }
+
+        private void TxtEditor_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            try
+            {
+                if (canChange)
+                {
+                    System.IO.File.WriteAllText(filePath, TxtEditor.Text);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                AppendListViewCalls(ex.Message);
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            //Save Path on closure  
+            Settings.Default.PathSetting = TxtDir.Text;
+            Settings.Default.Save();
         }
     }
 }
